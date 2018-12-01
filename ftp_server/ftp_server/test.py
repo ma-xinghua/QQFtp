ef57770c32fc2b874fac0a8136162bf3f604ee46
ma-xinghua <1290252467@qq.com>
2018-12-01 14:31:29 +0800

    Update test.py
    实现了对用户信息文件的写入：
        可判断用户名为空，两次输入密码不一致，未勾选权限，可创建文件夹
        不可处理用户名相同的问题
        不可在创建新用户之后清除所有输入和勾选

--- ftp_server/ftp_server/test.py
+++ ftp_server/ftp_server/test.py
@@ -1,7 +1,8 @@
 import sys
+import os
 from server import Ui_Dialog_server
 from PyQt5 import QtWidgets, QtGui
-from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import QFileDialog,QMessageBox
 from create import *
 from ftp_server import *
 
@@ -12,7 +13,48 @@ class createwindow(QtWidgets.QWidget, Ui_Dialog_create):
 
     # 定义槽函数
     def create(self):
-        print ("hahahanima")
+        self.userName=self.lineEdit.text()
+        self.userPasscode=self.lineEdit_2.text()
+        self.userPasscodeAgain=self.lineEdit_3.text()
+        self.Docupass=self.lineEdit_4.text()
+        #print (self.userName)
+        #print (self.userPasscode)
+        #print (self.userPasscodeAgain)
+        #print (self.Docupass)
+        self.authority=""
+        if (self.checkBox.isChecked()):
+            self.authority=self.authority+"e"
+        if (self.checkBox_2.isChecked()):
+            self.authority=self.authority+"l"
+        if (self.checkBox_3.isChecked()):
+            self.authority=self.authority+"r"
+        if (self.checkBox_4.isChecked()):
+            self.authority=self.authority+"d"
+        if (self.checkBox_5.isChecked()):
+            self.authority=self.authority+"a"
+        if (self.checkBox_6.isChecked()):
+            self.authority=self.authority+"m"
+        if (self.checkBox_7.isChecked()):
+            self.authority=self.authority+"f"
+        if (self.checkBox_8.isChecked()):
+            self.authority=self.authority+"w"
+        #print (self.authority)
+        if (self.userName==""):
+            warning3=QMessageBox.warning(self,"警告","用户名不能为空")
+            return
+        if (self.userPasscode!=self.userPasscodeAgain):
+            warning1=QMessageBox.warning(self,"警告","两次输入密码不一致")
+            return
+        if (self.authority==""):
+            warning2=QMessageBox.warning(self,"警告","必须选择一个权限")
+            return
+        if not os.path.exists(self.Docupass):
+               os.makedirs(self.Docupass) #如果不存在这个文件夹，就创建一个
+        file = open('userinfo.ini', 'a')
+        self.word = '\n'+self.userName+' '+self.userPasscode+' '+self.authority+' '+self.Docupass
+        file.write(self.word)
+        file.close()
+        information=QMessageBox.information(self,"提示","新用户成功创建")
 
 class mywindow(QtWidgets.QWidget, Ui_Dialog_server):
 
